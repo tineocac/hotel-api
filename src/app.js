fae16ead4f551b83d037ec06d0f2a264afad3f94
tineocac <carlostineocac@gmail.com>
2023-01-05 10:29:16 -0400

    feat: database and app structure were created

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const db = require("./utils/database");
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+
+db.authenticate()
+  .then(console.log("Succesfull aunthentication"))
+  .catch((error) => console.log(error));
+
+db.sync({ force: false })
+  .then(console.log("Succesfull synchronization"))
+  .catch((error) => console.log(error));
+
+app.get("/", (req, res) => {
+  console.log("Welcome to server");
+});
+
+module.exprots = app;
---
+++ src/utils/database.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const db = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER || "postgres",
+  port: process.env.DB_PORT || 5432,
+  host: process.env.DB_HOST || "localhost",
+  password: process.env.DB_PASSWORD || "root",
+  dialect: "postgres",
+  logging: false,
+});
+
+module.exports = db;
